b314219c6f828b8ed22fbfc65164d328f51fc06c
mishani0x0ef <mishani0x0ef@gmail.com>
2015-10-10 17:53:41 +0300

    Replaced bootstrap css with paper theme. Removed useless libraries and styles.

--- Jira.Extension.Chrome/js/optionsController.js
+++ Jira.Extension.Chrome/js/optionsController.js
@@ -18,4 +18,38 @@ jiraReporterApp.controller('OptionsController', function ($scope) {
     $scope.addSvnRepository = function () {
         
     };
+    
+    // Mock of method for saving settings.
+    // todo: finish that method with appropriate exception handling and saving. MR
+    $scope.saveSettings = function(settings){
+        chrome.runtime.lastError = null;
+    
+        chrome.storage.sync.set({
+            settings: settings
+            }, function(){        
+                if(typeof(chrome.runtime.lastError) !== 'undefined' && chrome.runtime.lastError !== null){
+                    message.message = chrome.runtime.lastError;
+                }
+            }
+        );
+    }
+
+    // Mock of method for getting settings.
+    // todo: finish that method with appropriate exception handling and getting. MR
+    $scope.getSettings = function(){    
+        chrome.storage.sync.get(["settings1", "settings2"], function(options){        
+        });
+    }
+
+    var notifyOptionsSaving = function(isSuccess, title, message){
+        new PNotify({
+            title: title,
+            text: message,
+            type: isSuccess ? 'success' : 'error',
+            cornerclass: 'ui-pnotify-sharp',
+            delay: 2000,
+            styling: 'bootstrap3',
+            icon: isSuccess ? 'glyphicon glyphicon-ok-circle' : 'glyphicon glyphicon-remove-circle'
+        });
+    }
 });
