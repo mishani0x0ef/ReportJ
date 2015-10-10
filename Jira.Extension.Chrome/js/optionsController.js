ab75a38f0a801ab49a05440be854ecb7e20c63f5
mishani0x0ef <mishani0x0ef@gmail.com>
2015-10-10 17:31:41 +0300

    Binded svn repositories markup to property in controller.

--- Jira.Extension.Chrome/js/optionsController.js
+++ Jira.Extension.Chrome/js/optionsController.js
@@ -1,5 +1,20 @@
 jiraReporterApp.controller('OptionsController', function ($scope) {
     
+    $scope.svnRepositories = [
+        { 
+            Name: "Awersome application repository",
+            Url: "http://repository.address/folder",
+            UserName: "Mark.Commit",
+            Password: "secret word"
+        },
+        { 
+            Name: "Repository for very important things",
+            Url: "http://svn.imporant-things/",
+            UserName: "Mark.Commit",
+            Password: "secret word"
+        }
+    ];
+    
     $scope.addSvnRepository = function () {
         
     };
