235d4e00e507b6d005d27e0cd3ce5a1abe6252d6
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2016-06-25 02:00:49 +0300

    #14 Added project with Tests for Repo lib. Create base infrastructure for tests.

---
+++ ReportJ.Flare/ReportJ.Flare.Repo.Tests/Services/CommitProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using ReportJ.Flare.Repo.Interfaces;
+using System.Net;
+
+namespace ReportJ.Flare.Repo.Tests.Services
+{
+    public abstract class CommitProviderTests<TProvider> : TestBase<ICommitProvider> where TProvider : ICommitProvider
+    {
+        private string _repoUrl;
+        private NetworkCredential _credentials;
+
+        protected override void PreSetUp()
+        {
+            base.PreSetUp();
+            ServiceLocator.Instance.Replace<ICommitProvider, TProvider>();
+        }
+
+        protected override void PostSetUp()
+        {
+            base.PostSetUp();
+            _repoUrl = "repo://test";
+            _credentials = new NetworkCredential("user", "password");
+        }
+
+        [Test]
+        public void GetLastCommitsTest()
+        {
+            Assert.That(() => TestUnit.GetLastCommits(_repoUrl, _credentials), Throws.Nothing);
+        }
+    }
+}
---
+++ ReportJ.Flare/ReportJ.Flare.Repo.Tests/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace ReportJ.Flare.Repo.Tests
+{
+    [TestFixture]
+    public abstract class TestBase<TUnit>
+    {
+        protected TUnit TestUnit { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            PreSetUp();
+
+            TestUnit = ServiceLocator.Instance.Resolve<TUnit>();
+
+            PostSetUp();
+        }
+
+        protected virtual void PreSetUp()
+        {
+        }
+
+        protected virtual void PostSetUp()
+        {
+        }
+    }
+}
