44ba40ef301eac971c509ec313fde9361794a931
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2016-06-26 15:09:19 +0300

    #14 Refactor Home page of API.

--- ReportJ.Flare/ReportJ.Flare.Api/Controllers/HomeController.cs
+++ ReportJ.Flare/ReportJ.Flare.Api/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ReportJ.Flare.Api.Controllers
 {
@@ -10,7 +6,7 @@ namespace ReportJ.Flare.Api.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "ReportJ.Flare";
 
             return View();
         }
