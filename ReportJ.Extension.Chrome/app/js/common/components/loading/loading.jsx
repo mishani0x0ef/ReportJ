4adea520e07958093f197bf7cf0bb59a943639cd
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-10-14 17:27:19 +0300

    #51 Improve display of loading component Add different icons to loading control, stretch loading

--- ReportJ.Extension.Chrome/app/js/common/components/loading/loading.jsx
+++ ReportJ.Extension.Chrome/app/js/common/components/loading/loading.jsx
@@ -7,14 +7,25 @@ import PropTypes from "prop-types";
 
 export class Loading extends Component {
     render() {
+        const icon = this._getIcon();
         return (
             <div className="app-loading">
                 <div className="app-loading-icon-container">
-                    <MaterialIcon className="app-loading-icon" icon="autorenew" />
+                    <MaterialIcon className="app-loading-icon" icon={icon} />
                 </div>
                 <div className="app-loading-text">{this.props.text}</div>
             </div>
         );
+    }
+
+    _getIcon() {
+        const icons = [
+            "favorite_border",
+            "child_care",
+            "sentiment_very_satisfied",
+        ];
+        const index = Math.floor(Math.random() * icons.length);
+        return icons[index];
     }
 }
 
--- ReportJ.Extension.Chrome/app/js/popup/components/commits/commits.jsx
+++ ReportJ.Extension.Chrome/app/js/popup/components/commits/commits.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import { Loading } from "app/js/common/components/loading/loading";
+
 export class Commits extends Component {
     render() {
         return (
-            <div>
-                Commits
+            <div className="app-list flex-stretch" aria-orientation="vertical">
+                <Loading text="Loading your lovely commits..."></Loading>
             </div>
         );
     }
