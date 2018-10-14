12370f7806a9a6c81d46bf317fda3845465ea2e7
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-10-14 17:52:36 +0300

    #51 Add success notification after template clicked

--- ReportJ.Extension.Chrome/app/js/popup/components/templates/template/template.jsx
+++ ReportJ.Extension.Chrome/app/js/popup/components/templates/template/template.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 
+import { Notification } from "app/js/common/components/notification/notification";
 import PropTypes from "prop-types";
 
 export class Template extends Component {
     constructor() {
         super();
-
         this._handleClick = this._handleClick.bind(this);
+        this.notification = React.createRef();
     }
 
     render() {
         return (
             <div onClick={this._handleClick} className="app-list-item" data-clicked="Added">
                 <span>{this.props.template.description}</span>
+                <Notification text="Added" ref={this.notification} />
             </div>
         );
     }
@@ -21,6 +23,7 @@ export class Template extends Component {
         if (typeof this.props.onClick === "function") {
             const template = this.props.template.description;
             this.props.onClick(template);
+            this.notification.current.show();
         }
     }
 }
