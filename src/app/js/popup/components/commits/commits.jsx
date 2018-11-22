90a6510e1f61d0d913bb406a1bfe1c38a1b52acf
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-11-22 23:16:24 +0200

    #51 avoid list collapse inside popup

--- src/app/js/popup/components/commits/commits.jsx
+++ src/app/js/popup/components/commits/commits.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 
 import { UnderDevelopment } from "app/js/common/components/under-development/under-development";
-import messages from "app/js/common/utils/messages";
 
 export class Commits extends Component {
     render() {
         return (
-            <div className="app-list flex-stretch" aria-orientation="vertical">
-                <UnderDevelopment />
-            </div>
+            <UnderDevelopment />
         );
     }
 }
