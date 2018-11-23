06c7f96dd4e8d13be2561a3ca0ad7ad49046c698
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-11-23 03:35:13 +0200

    #55 add pageviews to popup sections

--- src/app/js/popup/components/commits/commits.jsx
+++ src/app/js/popup/components/commits/commits.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
 import { UnderDevelopment } from "app/js/common/components/under-development/under-development";
+import { pageview } from "app/js/common/services/analytics";
 
 export class Commits extends Component {
+    constructor() {
+        super();
+        pageview("/popup/commits");
+    }
+
     render() {
         return (
             <UnderDevelopment />
