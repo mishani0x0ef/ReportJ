10cec716216cd1ba5f60c8921aa023bd07a8103f
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2023-07-29 18:39:24 +0300

    Enable extension only, when inside Jira page

--- extension/content/content.tsx
+++ extension/content/content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CloseIssue from './close-issue/close-issue';
 import AddIssueSummary from './add-issue-summary/add-issue-summary';
+import WhenInsideJira from './.common/components/when-inside-jira/when-inside-jira';
 
 const root = ReactDOM.createRoot(
   document.body.appendChild(document.createElement('reportj-extension'))
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <AddIssueSummary />
-    <CloseIssue />
+    <WhenInsideJira>
+      <AddIssueSummary />
+      <CloseIssue />
+    </WhenInsideJira>
   </React.StrictMode>
 );
