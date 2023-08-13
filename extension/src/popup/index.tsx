53c3750d6558a0b5822b9f47d6b2c0366f98f447
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2023-08-13 19:57:58 +0300

    Get rid of extra container element

--- extension/src/popup/index.tsx
+++ extension/src/popup/index.tsx
@@ -5,19 +5,16 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/header/header';
 import Preferences from './components/preferences/preferences';
 import Preference from './components/preference/preference';
-import Content from './components/content/content';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Content>
-      <Header />
+    <Header />
 
-      <Preferences>
-        <Preference name="test" description="test" />
-        <Preference name="test2" description="test2" />
-      </Preferences>
-    </Content>
+    <Preferences>
+      <Preference name="test" description="test" />
+      <Preference name="test2" description="test2" />
+    </Preferences>
   </React.StrictMode>
 );
