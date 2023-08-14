cde7e57b9ed51f872c1e6ca4faced43612058ca1
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2023-08-14 19:42:44 +0300

    Provide styles and structure for header

--- extension/src/popup/components/header/header.tsx
+++ extension/src/popup/components/header/header.tsx
@@ -1,8 +1,16 @@
+import './header.css';
 import React from 'react';
 
+const chromeWebStoreLink =
+  'https://chrome.google.com/webstore/detail/reportj/hijbdbjoelgicnhnghhhlkpbhjdmchfg';
+
 const Header: React.FC = () => (
-  <header>
-    <h1>ReportJ</h1>
+  <header className="header">
+    <img className="header__logo" src="../logo48.png" alt="ReportJ" />
+    <h2 className="header__title">ReportJ</h2>
+    <a className="header__link" target="__blank" href={chromeWebStoreLink}>
+      View in store
+    </a>
   </header>
 );
 
